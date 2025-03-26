160088487a4f241c3321074258fa4f12b0705837
Harsha-S2604 <harsha91148@outlook.com>
2025-03-27 00:11:05 +0530

    old changes

--- index.js
+++ index.js
@@ -2,36 +2,20 @@ require("dotenv").config({ path: `.env.${process.env.NODE_ENV || 'development'}`
 
 const express = require("express")
 const http = require('http');
-const https = require('https');
 const cors = require("cors")
-const fs = require('fs')
-const path = require('path')
 
 const contractRoutes = require("./routes/contracts")
 const services = require("./services");
 
-const keyPath = path.resolve(__dirname, './certs/selfsigned.key')
-const certPath = path.resolve(__dirname, './certs/selfsigned.crt')
-
-const key = fs.readFileSync(keyPath)
-const cert = fs.readFileSync(certPath)
-
-const options = {
-    key: key,
-    cert: cert
-}
-
 const app = express()
-app.use(cors({origin: "*"}))
+const server = http.createServer(app);
+const port = process.env.SERVER_PORT || 3000
+
+app.use(cors())
 app.use(express.json())
 app.use('/contracts', contractRoutes)
 
-const server = https.createServer(options, app);
-const port = process.env.SERVER_PORT || 3000
-
 server.listen(port, () => {
-    const protocol = server instanceof https.Server
-    console.log("protocol", protocol)
     services.init(server)
     global.appServices = services
 })
